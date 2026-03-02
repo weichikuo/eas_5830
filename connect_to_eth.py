d4083dbae92e5f5b40f96e30c52f9f154487cf69
weichikuo <87920535+weichikuo@users.noreply.github.com>
2026-03-02 23:40:42 +0800

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -26,7 +26,7 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	bnb_url = "wss://bsc-testnet-rpc.publicnode.com"
+	bnb_url = "https://bnb-testnet.api.onfinality.io/public"
 	w3 = Web3(HTTPProvider(bnb_url))
 	assert w3.is_connected(), f"Failed to connect to provider at {bnb_url}"
 
