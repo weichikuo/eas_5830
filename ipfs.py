d4395fdeb244b5af49e161350e8f10df8f042824
weichikuo <87920535+weichikuo@users.noreply.github.com>
2026-03-03 00:34:02 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -1,12 +1,14 @@
 import requests
 import json
 
+PINATA_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJlZmQxNDNkNS0yYzE5LTRmMDEtOTdhNi1hOTdlNmM4ODQ0MjAiLCJlbWFpbCI6ImVyaWMudy5rdW9AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiRlJBMSJ9LHsiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiTllDMSJ9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6ImVmZTU3NjA3MmYzZTdlZjU3NjRmIiwic2NvcGVkS2V5U2VjcmV0IjoiZTE2Y2YwYzEwZGViMmU1NmE3YzEwMzA4ZDExMzQ5ZWE2NDdhZmEyMDRiYmY1YTJmYzI0YjYzMmIxZjlkMGY4YyIsImV4cCI6MTgwNDAwNTA2NX0.vaD4dVZGjk8Rtw3cVsCVGoArtzkncmWVvHD8FgUPnMY"
+
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
 	url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
 	headers = {
-      "Authorization": "Bearer YOUR_PINATA_JWT",
+      "Authorization": f"Bearer {PINATA_JWT}",
       "Content-Type": "application/json"
 	}
 	response = requests.post(url, headers=headers, json=data)
