dcc8a5911348898988cac95ab8ec9bdecc93205f
weichikuo <87920535+weichikuo@users.noreply.github.com>
2026-03-22 13:54:26 +0800

    Update signatures.py

--- signatures.py
+++ signatures.py
@@ -7,13 +7,13 @@ def sign(m):
     w3 = Web3()
 
     # TODO create an account for signing the message
-    account_object = 0  # Create an Eth account
-    public_key = 0  # Eth account public key
-    private_key = 0  # Eth account private key
+    account_object = w3.eth.account.create()  # Create an Eth account
+    public_key = account_object.address  # Eth account public key
+    private_key = account_object.key  # Eth account private key
 
     # TODO sign the given message "m"
-    message = m  # Encode the message
-    signed_message = 0  # Sign the message
+    message = encode_defunct(text=m)  # Encode the message
+    signed_message = w3.eth.account.sign_message(message, private_key=private_key)  # Sign the message
 
 
     """You can save the account public/private keypair that prints in the next section
@@ -33,9 +33,9 @@ def verify(m, public_key, signed_message):
     w3 = Web3()
 
     # TODO verify the 'signed_message' is valid given the original message 'm' and the signers 'public_key'
-    message = m  # Encode the message
-    signer = 0  # Verify the message
-    valid_signature = 0  # True if message verifies, False if message does not verify
+    message = encode_defunct(text=m)  # Encode the message
+    signer = w3.eth.account.recover_message(message, signature=signed_message.signature)  # Verify the message
+    valid_signature = w3.to_checksum_address(signer) == w3.to_checksum_address(public_key)  # True if message verifies, False if message does not verify
 
     assert isinstance(valid_signature, bool), "verify should return a boolean value"
     return valid_signature
