410370bb638052e0c9e8550981ab873341ac1677
weichikuo <87920535+weichikuo@users.noreply.github.com>
2026-03-29 15:48:50 +0800

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -23,15 +23,29 @@ contract Destination is AccessControl {
     }
 
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		//YOUR CODE HERE
+		address wrapped = wrapped_tokens[_underlying_token];
+		require(wrapped != address(0), "token not registered");
+		BridgeToken(wrapped).mint(_recipient, _amount);
+		emit Wrap(_underlying_token, wrapped, _recipient, _amount);
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
+		address underlying = underlying_tokens[_wrapped_token];
+		require(underlying != address(0), "token not registered");
+		BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
+		emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		//YOUR CODE HERE
+		require(_underlying_token != address(0), "invalid underlying")
+		require(wrapped_tokens[_underlying_token] == address(0), "token already exists");
+		BridgeToken token = new BridgeToken(_underlying_token, name, symbol, address(this));
+		address wrapped = address(token);
+		wrapped_tokens[_underlying_token] = wrapped;
+		underlying_tokens[wrapped] = _underlying_token;
+		tokens.push(wrapped);
+		emit Creation(_underlying_token, wrapped);
+		return wrapped;
 	}
 
 }
