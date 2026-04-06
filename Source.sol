3ea2f454c7aff4f4ff4c426915efce403922d900
weichikuo <87920535+weichikuo@users.noreply.github.com>
2026-04-06 11:11:33 +0800

    Update Source.sol

--- Source.sol
+++ Source.sol
@@ -23,14 +23,24 @@ contract Source is AccessControl {
 
 	function deposit(address _token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE
+		require(approved[_token], "token not registered");
+		require(ERC20(_token).transferFrom(msg.sender, address(this), _amount), "transferFrom failed");
+		emit Deposit(_token, _recipient, _amount);
 	}
 
 	function withdraw(address _token, address _recipient, uint256 _amount ) onlyRole(WARDEN_ROLE) public {
 		//YOUR CODE HERE
+		require(approved[_token], "token not registered");
+		require(ERC20(_token).transfer(_recipient, _amount), "transfer failed");
+		emit Withdrawal(_token, _recipient, _amount);
 	}
 
 	function registerToken(address _token) onlyRole(ADMIN_ROLE) public {
 		//YOUR CODE HERE
+		require(!approved[_token], "token already registered");
+		approved[_token] = true;
+		tokens.push(_token);
+		emit Registration(_token);
 	}
 
 
